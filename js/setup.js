b5094ce89589885b62672eed25ffe472e22da5c2
Brunoandradel.a <brunoandradel.a.dev@gmail.com>
2022-04-24 19:25:13 -0300

    1.0.1 (botão "ASSISTIR TRAILER" finalizado)

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,42 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+let trailer = document.querySelector(".trailer");
+let btnFechar = document.querySelector(".btnFechar");
+let add = document.querySelector(".add");
+let remove = document.querySelector(".remove");
+
+function paraVideo() {
+  var videos = document.querySelectorAll("iframe, video");
+  Array.prototype.forEach.call(videos, function (video) {
+    if (video.tagName.toLowerCase() === "video") {
+      video.pause();
+    } else {
+      var src = video.src;
+      video.src = src;
     }
-})
+  });
+}
+
+add.addEventListener("click", () => {
+  trailer.classList.add("abrir");
+  });
+
+remove.addEventListener("click", () => {
+  btnFechar.parentNode.classList.remove("abrir");
+  paraVideo()
+});
